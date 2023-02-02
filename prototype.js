ce10b6b46cfb730ad9a2bfc060787c33e32c3a20
Dixita Patel <dixita.wappnet@gmail.com>
2023-02-02 13:29:21 +0530

    prototype

---
+++ prototype.js
@@ -0,0 +1,27 @@
+console.log("Hello, i am dixita patel");
+
+let a = {
+    name : 'dixita',
+    language : 'gujarati',
+    run : () => {
+       alert("self run")
+}
+}
+console.log(a);
+
+let p = {
+
+    run : () => {
+        alert("run")
+    }
+}
+
+a.__proto__ = p
+a.run()
+
+p.__proto__ = {
+
+    name2 : 'jay'
+}
+
+console.log(a.name2);
